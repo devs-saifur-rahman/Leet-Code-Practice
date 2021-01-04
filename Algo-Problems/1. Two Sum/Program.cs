59f889cf525f3c074d3e86e9f1bad5a67aad6138
Md. Saifur Rahman <devs.saifur.rahman@gmail.com>
2021-01-04 20:56:42 +0600

    Solution 1

---
+++ Algo-Problems/1. Two Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        //480 ms	31.3 MB
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int searchFor = target - nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (nums[j] == searchFor)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
