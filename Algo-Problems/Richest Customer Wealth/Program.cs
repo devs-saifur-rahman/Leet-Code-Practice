21ea3ef42c0b2c23e28b85bd5ed96e34b86b65cc
Md. Saifur Rahman <devs.saifur.rahman@gmail.com>
2021-01-04 19:19:40 +0600

    Richest Customer Wealth-init

---
+++ Algo-Problems/Richest Customer Wealth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Richest_Customer_Wealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
