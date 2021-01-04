93979809097cde24185875356fd97ef8b31efa66
Md. Saifur Rahman <devs.saifur.rahman@gmail.com>
2021-01-04 19:46:32 +0600

    Solution 1

---
+++ Algo-Problems/1472. Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1472.Kids_With_the_Greatest_Number_of_Candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        // 252 ms	31.3 MB
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            return candies.Select(x => (x + extraCandies) >= candies.Max()).ToList();
+        }
+    }
+}
