abe1f2b55da1bc09873b58bae1e6b9abc5c79054
Md. Saifur Rahman <devs.saifur.rahman@gmail.com>
2021-01-04 20:20:17 +0600

    solution 1

--- Algo-Problems/1470. Shuffle the Array/Program.cs
+++ Algo-Problems/1470. Shuffle the Array/Program.cs
@@ -11,5 +11,15 @@ namespace _1470.Shuffle_the_Array
         static void Main(string[] args)
         {
         }
+
+        //252 ms	33.6 MB
+        public int[] Shuffle(int[] nums, int n)
+        {
+
+            var firstHalf = nums.Select((x, i) => new { val = x, index = i }).Where((c, j) => j < n);
+            var secondHalf = nums.Select((x, i) => new { val = x, index = i - n }).Where((c, j) => j >= n);
+
+            return firstHalf.Concat(secondHalf).OrderBy(x => x.index).Select(x => x.val).ToArray();
+        }
     }
 }
