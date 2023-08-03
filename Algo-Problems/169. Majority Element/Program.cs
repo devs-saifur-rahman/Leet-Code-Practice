61c9236666dac5484f69d65c6fb96c8b0d883c22
Saifur Rahman <devs.saifur.rahman@gmail.com>
2023-08-04 01:37:09 +0600

    #169 - majority counter

---
+++ Algo-Problems/169. Majority Element/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _169.Majority_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int MajorityElement(int[] nums)
+        {
+            int maxElem = nums[0];
+            int maxCount = -1;
+
+            Dictionary<int,int> counterMap = new Dictionary<int,int>();
+
+            for(int idx =0; idx < nums.Length; idx++)
+            {
+                if (counterMap.ContainsKey(nums[idx]))
+                {
+                    counterMap[nums[idx]]++;
+                    if(nums.Length/2 <= counterMap[nums[idx]] && maxCount <= counterMap[nums[idx]])
+                    {
+                        maxElem = nums[idx];
+                        maxCount= counterMap[nums[idx]]; ;
+                    }
+                }
+                else
+                {
+                    counterMap.Add(nums[idx], 1);
+                }
+
+            }
+            
+            
+
+            return maxElem;
+
+        }
+    }
+}
