9bfab9adcdbfedc104c7cb15a2cfb6bcaf6fbdea
Saifur Rahman <devs.saifur.rahman@gmail.com>
2023-11-02 23:04:32 +0600

    #753  #739 - Stack problems

---
+++ Algo-Problems/735. Asteroid Collision/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _735.Asteroid_Collision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Program p = new Program();
+            int[] a = p.AsteroidCollision(new int[] { -2, 1, 1, -2 });
+
+            _ = Console.ReadLine();
+        }
+
+        public  int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> remainder = new Stack<int>();
+
+            foreach (var currVal in asteroids)
+            {
+                if (remainder.Count == 0 // NOTHING
+                    || currVal >= 0  // GOING RIGHT - POSITIVE FLOW
+                    || (currVal < 0 && remainder.Peek() < 0)) //LEFT BUT NO POSITIVE FLOW EXISTS HENCE NO COLLISION
+                {
+                    remainder.Push(currVal);
+                }
+                else
+                { // LEFT BUT POSITIVE FLOW TO RIGHT EXIST
+                    bool isPrevPos = false;
+                    bool shouldPush = false;
+                    int valueToPush = currVal;
+                    do
+                    {
+                        isPrevPos = false;
+                        if (Math.Abs(currVal) < Math.Abs(remainder.Peek()))
+                        {
+                            shouldPush = false;
+                            break;
+                        }
+                        else if (Math.Abs(currVal) == Math.Abs(remainder.Peek()))
+                        {
+                            remainder.Pop();
+                            shouldPush = false;
+                            break;
+                        }
+                        else
+                        {
+                            remainder.Pop();
+                            isPrevPos = remainder.Count > 0 ? remainder.Peek() > 0 : false;
+                            shouldPush = true;
+                        }
+                    } while (isPrevPos);
+                    if (shouldPush)
+                    {
+                        remainder.Push(valueToPush);
+                    }
+
+                }
+            }
+            var res = remainder.ToArray();
+            Array.Reverse(res);
+            return res;
+
+        }
+    }
+}
---
+++ Algo-Problems/739. Daily Temperatures/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _739.Daily_Temperatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] a = p.DailyTemperatures(new int[] { 89, 62, 70, 58, 47, 47, 46, 76, 100, 70 });
+            Console.WriteLine(String.Join(" , ", a ));
+            Console.ReadLine();
+        }
+
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            Stack<Tuple<int, int>> highestTemp = new Stack<Tuple<int, int>>();
+            int[] nextHighIdx = Enumerable.Repeat(-1, temperatures.Length).ToArray();
+            int curTemp = 0;
+            for (int i = temperatures.Length - 1; i >= 0; i--)
+            {
+                nextHighIdx[i] = i;
+                curTemp = temperatures[i];
+                if (highestTemp.Count == 0)
+                {
+                    highestTemp.Push(new Tuple<int, int>(i, curTemp));
+                }
+                else
+                {
+                    if (highestTemp.Peek().Item2 > curTemp)
+                    {
+                        nextHighIdx[i] = highestTemp.Peek().Item1;
+                        highestTemp.Push(new Tuple<int, int>(i, curTemp));
+                    }
+                    else
+                    {
+                        while (highestTemp.Count > 0 && highestTemp.Peek().Item2 <= curTemp)
+                        {
+                            highestTemp.Pop();
+                        }
+                        if (highestTemp.Count > 0)
+                        {
+                            nextHighIdx[i] = highestTemp.Peek().Item1;
+                        }
+                        highestTemp.Push(new Tuple<int, int>(i, curTemp));
+                    }
+                }
+            }
+            return nextHighIdx.Select((val, idx) => val - idx).ToArray();
+
+        }
+    }
+}
