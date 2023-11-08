266ec0021abb1b8b39997a9b96ab0815ef637eef
Saifur Rahman <devs.saifur.rahman@gmail.com>
2023-11-08 20:39:55 +0600

    #216, #17 Backtracking

---
+++ Algo-Problems/17. Letter Combinations of a Phone Number/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Letter_Combinations_of_a_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> vals = LetterCombinations("23");
+            
+            foreach (var item in vals)
+            {
+                Console.Write(item + " , ");
+            }
+            Console.ReadLine();
+        }
+
+        public void PrintContainer(Stack<Tuple<int, int, string>> container)
+        {
+            Console.WriteLine("-=-=-=-");
+            foreach (var item in container)
+            {
+                Console.WriteLine(item.Item1 + " - " + item.Item2 + " - " + item.Item3);
+            }
+            Console.WriteLine("-=-=-=-");
+        }
+        public static IList<string> LetterCombinations(string digits)
+        {
+            IList<string> result;
+            if (digits.Length == 0)
+            {
+                result = new List<string>();
+            }
+            else if (digits.Length == 1)
+            {
+                int val = digits[0] - '0';
+                Dictionary<int, IList<string>> dict = new Dictionary<int, IList<string>>();
+                dict.Add(2, new List<string> { "a", "b", "c" });
+                dict.Add(3, new List<string> { "d", "e", "f" });
+                dict.Add(4, new List<string> { "g", "h", "i" });
+                dict.Add(5, new List<string> { "j", "k", "l" });
+                dict.Add(6, new List<string> { "m", "n", "o" });
+                dict.Add(7, new List<string> { "p", "q", "r", "s" });
+                dict.Add(8, new List<string> { "t", "u", "v" });
+                dict.Add(9, new List<string> { "w", "x", "y", "z" });
+                result = dict[val];
+            }
+            else
+            {
+                IList<string> resultSecond = LetterCombinations(digits.Substring(1));
+                IList<string> resultFirst = LetterCombinations(digits[0].ToString());
+                result = (from x in resultFirst
+                         from y in resultSecond
+                         select x+ y).ToList();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algo-Problems/216. Combination Sum III/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _216.Combination_Sum_III
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            // IList<IList<int>> a = new List<IList<int>>();
+            //  var a = p.CombinationSum3(3, 7);
+            var b = p.CombinationSum3(6, 36);
+
+            Console.ReadLine();
+
+        }
+        public IList<IList<int>> CombinationSum3(int k, int n)
+        {
+            Tuple<bool, IList<IList<int>>> childRes = CombinationSum3(k, n, 0, n, 1);
+
+            return childRes.Item2;
+        }
+        public Tuple<bool, IList<IList<int>>> CombinationSum3(int k, int target, int covered, int remains, int lowest)
+        {
+            IList<IList<int>> retList = new List<IList<int>>();
+            bool isSuccess = false;
+
+            if (k == 0 && remains == 0)
+            {
+                isSuccess = true;
+                return new Tuple<bool, IList<IList<int>>>(isSuccess, retList);
+            }
+
+            if (target > 45 || target == 0 || (k <= 0 && remains > 0))
+            {
+                isSuccess = false;
+                return new Tuple<bool, IList<IList<int>>>(isSuccess, retList);
+            }
+
+            int nextN;
+            for (int i = lowest; i <= 9; i++)
+            {
+                nextN = remains - i;
+                if (nextN<0) {
+                    break;
+                }
+                Tuple<bool, IList<IList<int>>> childRes = CombinationSum3(k - 1, target, covered + i, nextN, i + 1);
+
+                if (childRes.Item1)
+                {
+                    IList<int> ls = new List<int>();
+
+                    if (childRes.Item2.Count == 0)
+                    {
+                        ls.Add(i);
+                        retList.Add(ls);
+                    }
+                    else
+                    {
+                        foreach (var itm in childRes.Item2)
+                        {
+                            ls = itm;
+                            ls.Add(i);
+                            retList.Add(ls);
+                        }
+                    }
+
+                    isSuccess = true;
+                }
+            }
+
+            return new Tuple<bool, IList<IList<int>>>(isSuccess, retList);
+        }
+    }
+}
+
+/*
+ Input: k = 3, n = 9
+Output: [[1,2,6],[1,3,5],[2,3,4]]
+Explanation:
+1 + 2 + 6 = 9
+1 + 3 + 5 = 9
+2 + 3 + 4 = 9
+ 
+ */
